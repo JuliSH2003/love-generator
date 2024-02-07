648ceb1b74a99c5e2373ab351f46d38825e4793e
Julia_Shuryakova <july.shuryakova@gmail.com>
2024-02-07 19:59:12 +0300

    generator

--- start/js.js
+++ start/js.js
@@ -2,13 +2,13 @@ const firstName = prompt ("Напишите первое имя");
 const secondName = prompt ("Напишите второе имя");
 const randomNumber = (Math.floor(Math.random() * 100));
 
-if (randomNumber < 33) {
+if (randomNumber < 33 && firstName != null && secondName != null && firstName != "" && secondName != "") {
     alert (`Результат совместимости имен ${firstName} и ${secondName} равен ${randomNumber} вам нужно поискать человека с другим именем.`)
-} else if (randomNumber >= 33 && randomNumber < 60){
+} else if (randomNumber >= 33 && randomNumber < 60 && firstName != null && secondName != null && firstName != "" && secondName != ""){
     alert (`Результат совместимости имен ${firstName} и ${secondName} равен ${randomNumber}Совместимость хорошая, но может быть и лучше.`)
-} else if (randomNumber >= 60){
+} else if (randomNumber >= 60 && firstName != null && secondName != null && firstName != "" && secondName != ""){
     alert (`Результат совместимости имен ${firstName} и ${secondName} равен ${randomNumber} Вы идеально подходите друг к другу.`)
 } else {
-    alert (`Упс.. какое-то странное число получилось. Попробуйте еще раз.`)
+    alert (`Упс.. Вы не ввели имя. Попробуйте еще раз.`)
 }
 
